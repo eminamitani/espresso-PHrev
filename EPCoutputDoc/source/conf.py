9a0a33104a9a381a82ed312ba118b982205876a9
Emi Minamitani <eminamitani@cello.t.u-tokyo.ac.jp>
2018-12-11 12:53:10 +0900

    use classic theme

--- EPCoutputDoc/source/conf.py
+++ EPCoutputDoc/source/conf.py
@@ -78,6 +78,9 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'classic'
+html_theme_options={
+    "rightsidebar":"true"
+    }
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
